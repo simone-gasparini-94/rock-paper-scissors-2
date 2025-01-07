ef80dff62b43bc6d8f486bf9300b59a68dd7ca28
simone-gasparini-94 <simone.gasparini.2@gmail.com>
2025-01-07 17:58:28 +0100

    Add playGame function
    Added a playGame function in which are nested all the other functions

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,14 @@
+let userScore = 0;
+let computerScore = 0;
+
 let computerChoice;
 let userChoice;
 
 function getUserChoice() {
     let answer = prompt("your choice:");
     userChoice = answer.toUpperCase();
-    console.log("your choice:")
-    console.log(userChoice);
-    getComputerChoice();
+    console.log(`user choice: ${userChoice}`);
+    return userChoice;
 }
 
 function getComputerChoice() {
@@ -18,9 +20,39 @@ function getComputerChoice() {
     } else {
         computerChoice = "SCISSORS";
     };
-    console.log("computer choice:")
-    console.log(computerChoice);
+    console.log(`computer choice: ${computerChoice}`)
+    return computerChoice;
 }
 
-getUserChoice();
+function playRound(userChoice, computerChoice) {
+    if (userChoice === computerChoice) {
+        console.log("DRAW");
+    } else if (userChoice === "ROCK" && computerChoice === "SCISSORS"
+                ||
+                userChoice === "PAPER" && computerChoice === "ROCK"
+                ||
+                userChoice === "SCISSORS" && computerChoice === "PAPER"
+                ) {
+                    ++userScore;
+                    console.log("WIN");
+                } else {
+                    ++computerScore;
+                    console.log("LOSS");
+                }
+    console.log(`USER SCORE: ${userScore}`)
+    console.log(`COMPUTER SCORE: ${computerScore}`)
+}
+
+function playGame() {
+    getUserChoice();
+    if(userChoice !== "ROCK" && userChoice !== "PAPER" && userChoice !== "SCISSORS") {
+        console.log("wrong input");
+        return;
+    }
+    getComputerChoice();
+    playRound();
+}
+
+playGame();
+
 
