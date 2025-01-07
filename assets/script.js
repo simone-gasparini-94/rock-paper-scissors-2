852dac6fa697fe03e6166f1fe4bdadbbcd02bc83
simone-gasparini-94 <simone.gasparini.2@gmail.com>
2025-01-07 15:55:25 +0100

    Translate random number into string ("ROCK", "PAPER", "SCISSORS")

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,14 @@
+let computerChoice;
+
 function getComputerChoice() {
-    const computerChoice = Math.floor(Math.random() * 3) + 1;
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
+    if(randomNumber === 1) {
+        computerChoice = "ROCK";
+    } else if(randomNumber === 2) {
+        computerChoice = "PAPER";
+    } else {
+        computerChoice = "SCISSORS";
+    };
     console.log(computerChoice);
 }
 
